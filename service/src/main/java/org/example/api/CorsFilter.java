130fabcd1edf6bec764f5ca39d85d3b4c2822643
RotoScoping <barahlush@melchior.localdomain>
2025-09-26 15:22:50 +0300

    feat(service): настройки cors фильтрации

---
+++ service/src/main/java/org/example/api/CorsFilter.java
@@ -0,0 +1,29 @@
+package org.example.api;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
+}
